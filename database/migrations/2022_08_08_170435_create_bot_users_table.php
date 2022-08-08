9a59eaae41dd2c939ea01d3dd1ae20f96ec5ba07
JavokhirMedia <raxmonberganovj@gmail.com>
2022-08-08 17:07:44 +0500

    first commit

---
+++ database/migrations/2022_08_08_170435_create_bot_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBotUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('region');
+            $table->string('district');
+            $table->string('phone_number');
+            $table->string('status');
+            $table->string('last_command');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
