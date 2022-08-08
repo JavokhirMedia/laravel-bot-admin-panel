a23b3f228bd9d43dbe2b7c2edf1561126fd0d282
JavokhirMedia <raxmonberganovj@gmail.com>
2022-08-08 16:42:25 +0500

    first commit

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -41,9 +41,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/about') }}">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('contact.create') }}">Contact</a>
-                </li>
             </ul>
         </div>
     </div>
